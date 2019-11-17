64d418f0816cd9566af8960fa87ded55c8732e64
qlwb <qlwb_family@163.com>
2019-11-17 19:10:05 +0800

    完成检查组中的分页查询功能（忘记提交controller类）

---
+++ health_backend/src/main/java/com/itheima/controller/CheckGroupController.java
@@ -0,0 +1,35 @@
+package com.itheima.controller;
+
+/**
+ * @Author: dxw
+ * @Date: 2019/11/17 17:37
+ */
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.itheima.entity.PageResult;
+import com.itheima.entity.QueryPageBean;
+import com.itheima.service.CheckGroupService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 体检检查组管理
+ */
+@RestController
+@RequestMapping("/checkgroup")
+public class CheckGroupController {
+    @Reference
+    private CheckGroupService checkGroupService;
+
+    //分页查询
+    @RequestMapping("/findPage")
+    public PageResult findPage(@RequestBody QueryPageBean queryPageBean){
+        PageResult pageResult = checkGroupService.pageQuery( queryPageBean.getCurrentPage(),
+                queryPageBean.getPageSize(), queryPageBean.getQueryString());
+        return pageResult;
+    }
+
+
+
+}
